53bfc925241724252c3618c4dadb68ea1c66b11f
DmytroMysak <dmytro.mysak@gmail.com>
2017-03-17 22:38:28 +0200

    add promise

--- allWork.js
+++ allWork.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         All Work
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  try to take over the world!
 // @author       You
 // @match        http://www.heroeswm.ru/map.php?*
@@ -9,7 +9,6 @@
 // ==/UserScript==
 'use strict';
 const url = `http://${location.hostname}/`;
-const config = { method: 'GET', credentials: 'same-origin'};
 const startTable = `<TABLE class=wb cellSpacing=0 cellPadding=3 width=500>`;
 const endTable = `</Table>`;
 
@@ -20,35 +19,20 @@ let insertIntoTable = (text) => {
   elem.innerHTML += text;
 };
 let httpGet = (url) => {
-  let xmlHttp = new XMLHttpRequest();
-  xmlHttp.open("GET", url, false);
-  xmlHttp.send(null);
-  return xmlHttp.responseText;
-};
-function makeRequest (url) {
-  return new Promise(function (resolve, reject) {
+  return new Promise((resolve, reject) => {
     let xhr = new XMLHttpRequest();
-    xhr.open('GET', url);
+    xhr.open("GET", url, true);
     xhr.onload = () => {
-      if (this.status >= 200 && this.status < 300) {
-        resolve(xhr.response);
+      if (xhr.status >= 200 && xhr.status < 300) {
+        resolve(xhr.responseText);
       } else {
-        reject({
-          status: this.status,
-          statusText: xhr.statusText
-        });
+        reject(`${startTable}ERROR${endTable}`);
       }
     };
-    xhr.onerror = function () {
-      reject({
-        status: this.status,
-        statusText: xhr.statusText
-      });
-    };
+    xhr.onerror = () => reject(`${startTable}ERROR${endTable}`);
     xhr.send();
   });
-}
-
+};
 let addWork = (text) => {
   const indexStartTable = text.indexOf(startTable);
   const indexEndTable = text.indexOf(endTable, indexStartTable);
@@ -64,16 +48,10 @@ if (hrefToRegion) {
   const extraction = `${hrefToRegion}&st=mn`;
   const treatment = `${hrefToRegion}&st=fc`;
   const production = `${hrefToRegion}&st=sh`;
-  addWork(httpGet(`${url}${production}`));
-  addWork(httpGet(`${url}${treatment}`));
-  addWork(httpGet(`${url}${extraction}`));
-  // fetch(`${url}${production}`, config)
-  //     .then(response => response.text()).then(text => addWork(text))
-  //     .then(() => fetch(`${url}${treatment}`, config))
-  //     .then(response => response.text()).then(text => addWork(text))
-  //     .then(() => fetch(`${url}${extraction}`, config))
-  //     .then(response => response.text())
-  //     .then(text => addWork(text));
-
-
+  httpGet(`${url}${production}`)
+      .then(text => addWork(text))
+      .then(() => httpGet(`${url}${treatment}`))
+      .then(text => addWork(text))
+      .then(() => httpGet(`${url}${extraction}`))
+      .then(text => addWork(text));
 }

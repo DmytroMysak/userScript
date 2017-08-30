a87f136a38e2c7719119e26dd2e826551c756218
DmytroMysak <dmytro.mysak@gmail.com>
2017-08-30 23:51:19 +0300

    add new script

---
+++ nodeVkAudio.js
@@ -0,0 +1,28 @@
+'use strict';
+const Promise = require('bluebird');
+const path = require('path');
+const fs = require('fs');
+const request = require('request');
+const mkdirp = require('mkdirp');
+
+const saveHere = path.join(__dirname, 'vkmusic');
+const songs = require('./urls.json').map(elem => {
+  elem.name = elem.name.replace(/\//g, '-')
+                       .replace(/|/g, '-')
+                       .replace(/&/g, '-')
+                       .replace(/"/g, `'`);
+  return elem;
+});
+mkdirp.sync(saveHere);
+Promise.each(songs, song => new Promise((resolve, reject) => {
+  const place = path.join(saveHere, song.name, '.mp3');
+  if (fs.existsSync(place)) {
+    console.log(`${song.name} already exist`);
+    resolve();
+  } else {
+    request(song.url)
+        .on('error', reject)
+        .pipe(fs.createWriteStream(place))
+        .on('finish', () => { console.log('Song downloaded: ' + song.name); resolve(); });
+  }
+})).catch(err => console.error('Failed: ' + err.message));
---
+++ workButton.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name         add button
+// @namespace    http://tampermonkey.net/
+// @version      0.3
+// @description  try to take over the world!
+// @author       You
+// @match        http://www.heroeswm.ru/recruit_event.php
+// @grant        none
+// ==/UserScript==
+
+'use strict';
+const inputs = document.getElementsByTagName('input');
+const login = 'LOGIN';
+const passwd = 'passwd';
+
+inputs.forEach(elem => {
+  if (elem.type === 'password') elem.value = passwd;
+  else if (elem.name.contains('usr')) elem.value = login
+});
